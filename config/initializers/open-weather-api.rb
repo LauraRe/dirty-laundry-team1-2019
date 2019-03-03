18e163ac056d608d539af0f7da953d6615dcfe61
Laura Reale <laaru1988@gmail.com>
2019-03-03 21:55:09 +0100

    add config key for open weather

--- config/initializers/open-weather-api.rb
+++ config/initializers/open-weather-api.rb
@@ -1,6 +1,6 @@
 OpenWeatherAPI.configure do |config|
     # API key
-    config.api_key = ''
+    config.api_key = '9f17e0746e709d22e5f04a5160c8abc2'
   
     # Optionals
     config.default_language = 'en'     # 'en' by default
